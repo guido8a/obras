d880b6d0b0bb1ca77fbb95a2897ac993d276ba83
guido8a <guido8a@gmail.com>
2020-04-23 16:43:17 -0500

    pruebas

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -20,7 +20,8 @@ environments {
         dataSource {
             dbCreate = "update"
 //            url = "jdbc:postgresql://192.168.100.9:5432/casa"
-            url = "jdbc:postgresql://127.0.0.1:5432/gadlr"
+//            url = "jdbc:postgresql://127.0.0.1:5432/gadlr"
+            url = "jdbc:postgresql://127.0.0.1:5432/gadlr_prba"
 //            url = "jdbc:postgresql://192.168.100.9:5432/casa_prba"
             username = "postgres"
             password = "postgres"
@@ -38,6 +39,34 @@ environments {
         dataSource {
             dbCreate = "update"
             url = "jdbc:postgresql://127.0.0.1:5432/gadlr"
+            username = "postgres"
+            password = "januslr"
+            properties {
+               // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
+               jmxEnabled = true
+               initialSize = 5
+               maxActive = 50
+               minIdle = 5
+               maxIdle = 25
+               maxWait = 10000
+               maxAge = 10 * 60000
+               timeBetweenEvictionRunsMillis = 5000
+               minEvictableIdleTimeMillis = 60000
+               validationQuery = "SELECT 1"
+               validationQueryTimeout = 3
+               validationInterval = 15000
+               testOnBorrow = true
+               testWhileIdle = true
+               testOnReturn = false
+               jdbcInterceptors = "ConnectionState"
+               defaultTransactionIsolation = java.sql.Connection.TRANSACTION_READ_COMMITTED
+            }
+        }
+    }
+    pruebas {
+        dataSource {
+            dbCreate = "update"
+            url = "jdbc:postgresql://127.0.0.1:5432/gadlr_prba"
             username = "postgres"
             password = "januslr"
             properties {
