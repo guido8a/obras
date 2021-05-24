aeac937272dc928fdd04fc29452600f579d5089e
guido8a <guido8a@gmail.com>
2021-05-24 15:00:21 -0500

    leeCSV

--- grails-app/controllers/janus/InicioController.groovy
+++ grails-app/controllers/janus/InicioController.groovy
@@ -70,7 +70,7 @@ class InicioController extends janus.seguridad.Shield {
 
     /** carga datos desde un CSV - utf-8: si ya existe lo actualiza
      * */
-    def leeCSV() {
+    def leeCSV_lr() {
 //        println ">>leeCSV.."
         def contador = 0
         def cn = dbConnectionService.getConnection()
@@ -171,10 +171,10 @@ class InicioController extends janus.seguridad.Shield {
 //            println "sql: $sql"
 
             try {
-                    cn.execute(sql.toString())
-                    if (cn.updateCount > 0) {
-                        insertados++
-                    }
+                cn.execute(sql.toString())
+                if (cn.updateCount > 0) {
+                    insertados++
+                }
             } catch (Exception ex) {
                 repetidos++
                 println "Error al insertar $ex"
@@ -183,6 +183,141 @@ class InicioController extends janus.seguridad.Shield {
         }
         cnta++
         return [errores: errores, insertados: insertados, repetidos: repetidos]
+    }
+
+    /** carga datos desde un CSV - utf-8: si ya existe lo actualiza
+     * */
+    def leeCSV() {
+        println ">>leeItems.."
+        def contador = 0
+        def cn = dbConnectionService.getConnection()
+        def estc
+        def rgst = []
+        def cont = 0
+        def repetidos = 0
+        def procesa = 5
+        def crea_log = false
+        def inserta
+        def fcha
+        def magn
+        def sqlp
+        def directorio
+//        def tipo = 'prueba'
+        def tipo = 'prod'
+
+        if (tipo == 'prueba') { //botón: Cargar datos Minutos
+            procesa = 5
+            crea_log = false
+        } else {
+            procesa = 100000000000
+            crea_log = true
+        }
+
+        def nmbr = ""
+        def arch = new File('/home/guido/proyectos/venta-servicios/data/items.csv')
+        def cuenta = 0
+        def line
+        arch.withReader { reader ->
+            while ((line = reader.readLine()) != null) {
+                if (cuenta > 0 && cuenta < procesa) {
+
+                    rgst = line.split('\t')
+                    rgst = rgst*.trim()
+
+                    inserta = cargaItems(rgst)
+                    cont += inserta.insertados
+                    repetidos += inserta.repetidos
+
+                    cuenta++
+                } else {
+                    cuenta++
+                }
+            }
+            println "---> archivo: ${arch.toString()} --> cont: $cont, repetidos: $repetidos"
+            render "Se han cargado ${cont} líneas de datos y han existido : <<${repetidos}>> repetidos"
+        }
+    }
+
+    def cargaItems(rgst) {
+        def errores = ""
+        def cnta = 0
+        def insertados = 0
+        def repetidos = 0
+        def cn = dbConnectionService.getConnection()
+        def sqlsbgr = ""
+        def sql = ""
+        def grpo = 0, sbgr = 0, dprt = 0, undd = 0, item = 0, cdgo = ""
+        def fcha = ""
+        def id = 0
+        def resp = 0
+
+        println "\n inicia cargado de datos para $rgst"
+        cnta = 0
+        if (rgst[1].toString().size() > 0) {
+            grpo = rgst[0] == 'M' ? '1' : rgst[0] == 'MO' ? '2' : '3'
+            undd = rgst[0] == 'u' ? '22' : rgst[0] == 'm' ? '12' : '6'
+            sqlsbgr = "select sbgr__id from sbgr where grpo__id = $grpo and sbgrcdgo ilike '${rgst[1].toString().trim()}'"
+            println "sqlSbgr: $sqlsbgr"
+            sbgr = cn.rows(sqlsbgr.toString())[0]?.sbgr__id
+
+            println "grpo: $grpo, sbgr: $sbgr"
+            if (sbgr) {
+                cdgo = "${rgst[1].toString().trim()}.${rgst[2].toString().trim()}.${completa(rgst[3].toString().trim())}"
+                sqlsbgr = "select dprt__id from dprt where sbgr__id = $sbgr and dprtcdgo ilike '%${rgst[2]}%'"
+                dprt = cn.rows(sqlsbgr.toString())[0]?.dprt__id
+                println "sqlsbgr: $sqlsbgr"
+                println "dprt: ${rgst[2]} --> dprt__id: ${dprt}"
+
+                if (dprt) {
+                    sqlsbgr = "insert into item(item__id, undd__id, tpit__id, dprt__id, itemcdgo, itemnmbr," +
+                            "itempeso, itemtrps, itemtrvl, itemrndm, tpls__id) " +
+                            "values (default, ${undd}, 1, ${dprt}, '${cdgo}', '${rgst[4].toString().trim()}', " +
+                            "0,0,0,0, 1) returning item__id"
+                    println "--> $sqlsbgr"
+                    cn.eachRow(sqlsbgr.toString()) { d ->
+                        item = d.item__id
+                    }
+                    println "item --> $item"
+                }
+            }
+
+            rgst[6] = rgst[6] ?: ''
+            println "precio: ${rgst[6]}, ${rgst[6]?.size()}"
+            if (rgst[6]?.size() > 2) {
+                sql = "select count(*) nada from rbpc where item__id = ${item}"
+                cnta = cn.rows(sql.toString())[0]?.nada
+                println "sql ---> ${sql}"
+                def lgar = (grpo == '1' ? 2 : 4)
+                if (item && (cnta == 0)) {
+                    /* crea la precio */
+                    sql = "insert into rbpc (rbpc__id, item__id, lgar__id, rbpcfcha, rbpcpcun, rbpcfcin, " +
+                            "rbpcrgst) " +
+                            "values(default, ${item}, ${lgar}, '1-may-2021', ${rgst[6]}, '1-may-2021', 'N') " +
+                            "returning rbpc__id"
+                    println "sql ---> ${sql}"
+
+                    try {
+                        cn.eachRow(sql.toString()) { d ->
+                            id = d.rbpc__id
+                            insertados++
+                        }
+                    } catch (Exception ex) {
+                        repetidos++
+//                    println "Error taller $ex"
+                        println "Error rbpc ${rgst[6]}"
+//                    println "sql: $sql"
+                    }
+                }
+            }
+        }
+
+        cnta++
+        return [errores: errores, insertados: insertados, repetidos: repetidos]
+    }
+
+    def completa(tx) {
+        def ln = tx.size()
+        return '0' * (3 - ln) + tx
     }
 
 
